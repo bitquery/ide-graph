c815d6855520de4267d44bf79b48a3817ace1288
Andrey Ivanov <ivanov17andrey@gmail.com>
2021-01-22 06:52:57 +0300

    supportsModel checks all properties

---
+++ src/checkAllProperties.js
@@ -0,0 +1,31 @@
+export default function checkAllProperties(props, model) {
+  const availableProps = new Set()
+  function addProp(pre, item) {
+    if (item.selectionSet) {
+      item.selectionSet.selections.forEach((i) => {
+				const start = pre ? pre + '.' : ''
+        availableProps.add(start + item.name.value)
+        addProp(start + item.name.value, i)
+      })
+    } else {
+			const start = pre ? pre + '.' : ''
+      availableProps.add(start + item.name.value)
+    }
+  }
+  if (model.selectionSet) {
+    model.selectionSet.selections.forEach((item) => {
+      addProp('', item)
+    })
+	}
+	
+  function isSuperset(set, subset) {
+    for (var elem of subset) {
+      if (!set.has(elem)) {
+        return false
+      }
+    }
+    return true
+	}
+	console.log(availableProps)
+  return isSuperset(availableProps, new Set(props))
+}
--- src/index.js
+++ src/index.js
@@ -2,34 +2,117 @@ import SankeyEditor from './reactComponents/SankeyEditor'
 import SankeyRenderer from './reactComponents/SankeyRenderer'
 import GraphEditor from './reactComponents/GraphEditor'
 import GraphRenderer from './reactComponents/GraphRenderer'
-
+import checkAllProperties from './checkAllProperties'
 
 class SankeyPlugin {
-	constructor() {
-		this.id = 'sankey'
-		this.name = 'Sankey'
-		this.editor = SankeyEditor
-		this.renderer = SankeyRenderer
-	}
-	supportsModel(model) {
-		for (let key in model) {
-			return !key.includes('.') || model[key].typeInfo.toString()[0]==='[' && model[key].typeInfo.toString().slice(-2, -1)!=='0'
-		}
-	}
+  constructor() {
+    this.id = 'sankey'
+    this.name = 'Sankey'
+    this.editor = SankeyEditor
+    this.renderer = SankeyRenderer
+  }
+  supportsModel(model) {
+    for (let key in model) {
+			let isLack = false
+      let coinpaths = []
+
+      if (!key.includes('.')) {
+        coinpaths.push(...model[key].selectionSet.selections)
+      } else if (
+        model[key].typeInfo.toString()[0] === '[' &&
+        model[key].typeInfo.toString().slice(-2, -1) !== '0'
+      ) {
+        coinpaths.push(model[key])
+      } else {
+        return false
+      }
+
+      const properties = [
+        'sender',
+        'sender.address',
+        'sender.annotation',
+        'sender.smartContract',
+        'sender.smartContract.contractType',
+        'sender.smartContract.currency',
+        'sender.smartContract.currency.symbol',
+        'sender.smartContract.currency.name',
+        'receiver',
+        'receiver.address',
+        'receiver.annotation',
+        'receiver.smartContract',
+        'receiver.smartContract.contractType',
+        'receiver.smartContract.currency',
+        'receiver.smartContract.currency.symbol',
+        'receiver.smartContract.currency.name',
+        'amount',
+        'currency',
+        'currency.symbol',
+        'depth',
+        'count',
+      ]
+
+      coinpaths.forEach((item) => {
+				if (!checkAllProperties(properties, item)) isLack = true
+			})
+
+			return !isLack
+    }
+  }
 }
 
 class GraphPlugin {
-	constructor() {
-		this.id = 'graph'
-		this.name = 'Graph'
-		this.editor = GraphEditor
-		this.renderer = GraphRenderer
-	}
-	supportsModel(model) {
-		for (let key in model) {
-			return !key.includes('.') || model[key].typeInfo.toString()[0]==='[' && model[key].typeInfo.toString().slice(-2, -1)!=='0'
-		}
-	}
+  constructor() {
+    this.id = 'graph'
+    this.name = 'Graph'
+    this.editor = GraphEditor
+    this.renderer = GraphRenderer
+  }
+  supportsModel(model) {
+    for (let key in model) {
+			let isLack = false
+      let coinpaths = []
+
+      if (!key.includes('.')) {
+        coinpaths.push(...model[key].selectionSet.selections)
+      } else if (
+        model[key].typeInfo.toString()[0] === '[' &&
+        model[key].typeInfo.toString().slice(-2, -1) !== '0'
+      ) {
+        coinpaths.push(model[key])
+      } else {
+        return false
+      }
+      const properties = [
+        'sender',
+        'sender.address',
+        'sender.annotation',
+        'sender.smartContract',
+        'sender.smartContract.contractType',
+        'sender.smartContract.currency',
+        'sender.smartContract.currency.symbol',
+        'sender.smartContract.currency.name',
+        'receiver',
+        'receiver.address',
+        'receiver.annotation',
+        'receiver.smartContract',
+        'receiver.smartContract.contractType',
+        'receiver.smartContract.currency',
+        'receiver.smartContract.currency.symbol',
+        'receiver.smartContract.currency.name',
+        'amount',
+        'currency',
+        'currency.symbol',
+        'depth',
+        'count',
+      ]
+
+      coinpaths.forEach((item) => {
+				if (!checkAllProperties(properties, item)) isLack = true
+			})
+
+			return !isLack
+    }
+  }
 }
 
 export let graphPlugins = [new SankeyPlugin(), new GraphPlugin()]
