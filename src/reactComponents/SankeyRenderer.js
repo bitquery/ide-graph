c2d3c1847b96e455ae216f3e56d192f322cf09f1
Andrey Ivanov <ivanov17andrey@gmail.com>
2021-02-12 21:36:12 +0300

    renderer added children

--- src/reactComponents/SankeyRenderer.js
+++ src/reactComponents/SankeyRenderer.js
@@ -1,7 +1,7 @@
 import React, { useEffect } from 'react'
 import { address_sankey } from '../lib'
 
-function SankeyRenderer({ el, config, dataSource, displayedData }) {
+function SankeyRenderer({ el, config, dataSource, displayedData, children }) {
   useEffect(() => {
     if (dataSource && config && config.data && dataSource.data) {
       try {
@@ -16,9 +16,10 @@ function SankeyRenderer({ el, config, dataSource, displayedData }) {
   }, [JSON.stringify(config), JSON.stringify(dataSource)])
   if (!dataSource) return <div></div>
   return (
-    <div className="widget-display">
+    <>
+      {children}
       <div style={{ width: '100%', overflowY: 'hidden' }} id={el} key={el} />
-    </div>
+    </>
   )
 }
 
